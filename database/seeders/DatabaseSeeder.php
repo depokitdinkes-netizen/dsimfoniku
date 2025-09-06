5a5d1ad934e45344283f2c0cd9698191340e1b9e
niit1 <niit1@hotmail.com>
2025-09-06 22:24:13 +0700

    benerin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         User::create([
             'fullname' => 'Superadmin',
-            'email' => 'superadmin@gmail.com',
-            'password' => Hash::make('passwordnyabukanpassword'),
+            'email' => 'super@gmail.com',
+            'password' => Hash::make('123456'),
             'role' => 'SUPERADMIN',
             'sizebaris1' => '20px',
             'baris1' => 'PEMERINTAHAN KOTA DEPOK',
@@ -27,41 +27,6 @@ class DatabaseSeeder extends Seeder
             'baris4' => 'Telp : (021) 29402281 DEPOK 16431',
         ]);
 
-        $adminSukamaju = User::create([
-            'fullname' => 'Admin Puskesmas Sukamaju',
-            'email' => 'admin.sukamaju@gmail.com',
-            'password' => Hash::make('admin12345'),
-            'role' => 'ADMIN',
-            'sizebaris1' => '18px',
-            'baris1' => 'PEMERINTAHAN KOTA DEPOK',
-            'sizebaris2' => '30px',
-            'baris2' => 'PUSKESMAS SUKAMAJU',
-            'sizebaris3' => '13px',
-            'baris3' => 'Jl. Sukamaju No. 10, Depok',
-            'sizebaris4' => '13px',
-            'baris4' => 'Telp : (021) 12345678 DEPOK 16431',
-            'sizebaris5' => '13px',
-            'baris5' => 'Email: sukamaju@puskesmas.depok.go.id',
-        ]);
-
-        // Add multiple kelurahan for admin Sukamaju
-        UserKelurahan::create([
-            'user_id' => $adminSukamaju->id,
-            'kelurahan' => 'Sukamaju Baru',
-            'kecamatan' => 'Tapos'
-        ]);
-
-        UserKelurahan::create([
-            'user_id' => $adminSukamaju->id,
-            'kelurahan' => 'Jatijajar',
-            'kecamatan' => 'Tapos'
-        ]);
-
-        UserKelurahan::create([
-            'user_id' => $adminSukamaju->id,
-            'kelurahan' => 'Cilodong',
-            'kecamatan' => 'Tapos'
-        ]);
 
         // Sample data sekolah
         
